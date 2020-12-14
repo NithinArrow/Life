bc8c2d344d700c648cab884114d8eef8737bab55
Nithin V <nithin_arrow@outlook.com>
2020-12-14 10:23:59 +0530

    Finished Author Routes

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
-const expressEjsLayouts = require("express-ejs-layouts")
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
@@ -16,6 +16,7 @@ app.set('view engine','ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended:false }))
 
